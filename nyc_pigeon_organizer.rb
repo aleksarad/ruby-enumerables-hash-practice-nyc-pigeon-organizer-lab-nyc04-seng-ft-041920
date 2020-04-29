456d085a0f8111a9a015177e96929c910438e218
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-29 14:55:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key, value|
+    p value
     # value.each do |innerkey, innervalue|
     #   innervalue.each do |item|
     #     p item
